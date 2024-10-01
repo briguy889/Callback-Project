bca7e374b8f8eb925273ed5488119d33cca60f9f
briguy889 <j7702914@gmail.com>
2024-10-01 15:30:14 -0700

    In progress

--- long-practice copy/try-out-callbacks/problems/04-my-filter.js
+++ long-practice copy/try-out-callbacks/problems/04-my-filter.js
@@ -20,9 +20,23 @@ console.log(result2);      // ['choose', 'words', 'only']
 *******************************************************************************/
 
 function myFilter(array, cb) {
-    // Your code here 
+    let arr = []
+    for (let elem of array) {
+        if (cb(elem)) {
+            arr.push(elem)
+        }
+    }
+    return arr
 }
+let result1 = myFilter([5, 7, 4, 3, 8], function (n) {
+    return n % 2 === 0;
+});
+console.log(result1);       // [ 4, 8 ]
 
+let result2 = myFilter(['choose', 'big', 'words', 'only'], function (s) {
+    return s.length > 3;
+});
+console.log(result2);      // ['choose', 'words', 'only']
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 try {
     module.exports = myFilter;
